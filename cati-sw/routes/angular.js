b515dd3154e5b8c03d93bdf0d32e11c51eaedb05
AngheloAlf <anghelo.carvajal.14@sansano.usm.cl>
2016-10-28 16:27:49 -0300

    fix #8

--- cati-sw/routes/angular.js
+++ cati-sw/routes/angular.js
@@ -2,32 +2,41 @@
  * Created by Anghelo on 27-10-2016.
  */
 
-// TODO: put some security
+var index = require('../routes/index');
+var User = require('../models/user.js');
+var Client = require('../models/client.js');
+var Project = require('../models/project.js');
 
 exports.getUsers = function(req, res){
-    var User = require('../models/user.js');
-    User.sendAllUsers(req, res);
+    index.verificateLogin(req, res, true, function(req, res){
+        User.sendAllUsers(req, res);
+    });
 };
 exports.getUserData = function(req, res){
-    var User = require('../models/user.js');
-    User.sendUserById(req, res, req.params.idUser);
+    index.verificateLogin(req, res, true, function(req, res){
+        User.sendUserById(req, res, req.params.idUser);
+    });
 };
 
 exports.getClients = function(req, res){
-    var Client = require('../models/client.js');
-    Client.sendAllClients(req, res);
+    index.verificateLogin(req, res, true, function(req, res) {
+        Client.sendAllClients(req, res);
+    });
 };
 exports.getClientData = function(req, res){
-    var Client = require('../models/client.js');
-    Client.sendClientById(req, res, req.params.idClient);
+    index.verificateLogin(req, res, true, function(req, res) {
+        Client.sendClientById(req, res, req.params.idClient);
+    });
 };
 
 exports.getProjects = function(req, res){
-    var Project = require('../models/project.js');
-    Project.sendAllProjects(req, res);
+    index.verificateLogin(req, res, true, function(req, res) {
+        Project.sendAllProjects(req, res);
+    });
 };
 exports.getProjectData = function(req, res){
-    var Project = require('../models/project.js');
-    Project.sendProjectById(req, res, req.params.idProject);
+    index.verificateLogin(req, res, true, function(req, res) {
+        Project.sendProjectById(req, res, req.params.idProject);
+    });
 };
 
