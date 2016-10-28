c7d131898ad04e23cd0f1afb13272bdc95e2188d
AngheloAlf <anghelo.carvajal.14@sansano.usm.cl>
2016-10-28 16:46:29 -0300

    fix the user cannot log in

--- cati-sw/routes/user.js
+++ cati-sw/routes/user.js
@@ -11,21 +11,30 @@ exports.userInterface = function(req, res){
     }*/
     //index.verificateLogin(req, res, false, 'userDash', { title: 'CATI - Encuestador', nombre: req.session.userData.userName });
 
-    index.verificateLogin(req, res, true, function(req, res){
+    index.verificateLogin(req, res, false, function(req, res){
         res.render('userDash', { title: 'CATI - Encuestador', nombre: req.session.userData.userName });
-    })
+    });
 
+};
+
+exports.changePassInterface = function(req, res){
+    /*if(req.session.userData && !req.session.userData.admin){ //If user is connected
+     res.render('updateUserDashUser', { title: 'CATI - Encuestador - Cambiar contraseña', nombre: req.session.userData.userName });
+     }
+     else{//else, redirects to the login interface
+     res.redirect('/login');
+     }*/
+    //index.verificateLogin(req, res, false, 'updateUserDashUser', { title: 'CATI - Encuestador - Cambiar contraseña', nombre: req.session.userData.userName });
+    index.verificateLogin(req, res, false, function(req, res){
+        res.render('updateUserDashUser', { title: 'CATI - Encuestador - Cambiar contraseña', nombre: req.session.userData.userName });
+    });
 };
 
 //user form handler
 exports.processForm = function(req, res){
-    if(req.session.userData && !req.session.userData.admin) { //If user is connected
+    /*if(req.session.userData && !req.session.userData.admin) { //If user is connected
         if(req.body.submitButton == "changePass"){ //Change the pass interviewer
-            console.log("la wea weon qlo");
-            console.log(req.body.interNewPass);
-            console.log(req.body.interNewPass2);
             if(req.body.interNewPass == req.body.interNewPass2){
-                console.log("la wea weon qlo");
                 var User = require('../models/user.js');
                 User.changePass(req.session.userData.userID, req.body.interOldPass, req.body.interNewPass);
             }
@@ -34,20 +43,14 @@ exports.processForm = function(req, res){
     }
     else{//else, redirects to the login interface
         res.redirect('/login');
-    }
-};
-
-exports.changePassInterface = function(req, res){
-    /*if(req.session.userData && !req.session.userData.admin){ //If user is connected
-        res.render('updateUserDashUser', { title: 'CATI - Encuestador - Cambiar contraseña', nombre: req.session.userData.userName });
-    }
-    else{//else, redirects to the login interface
-        res.redirect('/login');
     }*/
-    //index.verificateLogin(req, res, false, 'updateUserDashUser', { title: 'CATI - Encuestador - Cambiar contraseña', nombre: req.session.userData.userName });
-
-    index.verificateLogin(req, res, true, function(req, res){
-        res.render('updateUserDashUser', { title: 'CATI - Encuestador - Cambiar contraseña', nombre: req.session.userData.userName });
-    })
-
+    index.verificateLogin(req, res, false, function(req, res){
+        if(req.body.submitButton == "changePass"){ //Change the pass interviewer
+            if(req.body.interNewPass == req.body.interNewPass2){
+                var User = require('../models/user.js');
+                User.changePass(req.session.userData.userID, req.body.interOldPass, req.body.interNewPass);
+            }
+        }
+        res.redirect('/user');
+    });
 };
