a940c1c0db93bb55e6ca7478d099717a91fc066a
AngheloAlf <anghelo.carvajal.14@sansano.usm.cl>
2016-11-02 12:01:06 -0300

    deleting some unused code

--- cati-sw/routes/admin.js
+++ cati-sw/routes/admin.js
@@ -12,105 +12,37 @@ exports.adminInterface = function(req, res){
 };
 
 exports.createProyectInterface = function(req, res){
-    /*if(req.session.userData && req.session.userData.admin){ //If admin is connected
-        res.render('createProyectDashAdmin', { title: 'CATI - Admin - Crear Proyecto', nombre: req.session.userData.userName, clientsList: req.session.clients});
-    }
-    else{//else, redirects to the login interface
-        res.redirect('/login');
-    }*/
-    //index.verificateLogin(req, res, true, 'createProyectDashAdmin', { title: 'CATI - Admin - Crear Proyecto', nombre: req.session.userData.userName, clientsList: req.session.clients});
     index.verificateLogin(req, res, true, function(req, res){
         res.render('createProyect', { title: 'CATI - Admin - Crear Proyecto', nombre: req.session.userData.userName});
     });
 };
 
 exports.createUserInterface = function(req, res){
-    /*if(req.session.userData && req.session.userData.admin){ //If admin is connected
-        res.render('createUserDashAdmin', { title: 'CATI - Admin - Crear Encuestador', nombre: req.session.userData.userName});
-    }
-    else{//else, redirects to the login interface
-        res.redirect('/login');
-    }*/
-    //index.verificateLogin(req, res, true, 'createUserDashAdmin', { title: 'CATI - Admin - Crear Encuestador', nombre: req.session.userData.userName});
     index.verificateLogin(req, res, true, function(req, res){
         res.render('createUser', { title: 'CATI - Admin - Crear Encuestador', nombre: req.session.userData.userName});
     });
 };
 
 exports.deleteUserInterface = function(req, res){
-    /*if(req.session.userData && req.session.userData.admin){ //If admin is connected
-        res.render('deleteUser', { title: 'CATI - Admin - Eliminar Encuestador', nombre: req.session.userData.userName, userList: req.session.users});
-    }
-    else{//else, redirects to the login interface
-        res.redirect('/login');
-    }*/
-    //index.verificateLogin(req, res, true, 'deleteUser', { title: 'CATI - Admin - Eliminar Encuestador', nombre: req.session.userData.userName, userList: req.session.users});
     index.verificateLogin(req, res, true, function(req, res){
         res.render('deleteUser', { title: 'CATI - Admin - Eliminar Encuestador', nombre: req.session.userData.userName, userList: req.session.users});
     });
 };
 
 exports.uploadCSVInterface = function(req, res){
-    /*if(req.session.userData && req.session.userData.admin){ //If admin is connected
-        res.render('uploadCSV', { title: 'CATI - Admin - Subir contactos', nombre: req.session.userData.userName, projectList: req.session.AllProjects});
-    }
-    else{//else, redirects to the login interface
-        res.redirect('/login');
-    }*/
-    //index.verificateLogin(req, res, true, 'uploadCSV', { title: 'CATI - Admin - Subir contactos', nombre: req.session.userData.userName, projectList: req.session.AllProjects});
     index.verificateLogin(req, res, true, function(req, res){
         res.render('uploadCSV', { title: 'CATI - Admin - Subir contactos', nombre: req.session.userData.userName});
     })
 };
 
 exports.readUsersInterface = function(req, res){
-    //index.verificateLogin(req, res, true, 'readUsers', { title: 'CATI - Admin - Ver encuestadores', nombre: req.session.userData.userName});
     index.verificateLogin(req, res, true, function(req, res){
         res.render('readUsers', { title: 'CATI - Admin - Ver encuestadores', nombre: req.session.userData.userName});
     })
 };
 
 //admin form handler
-exports.processForm = function (req, res){
-    /*if(req.session.userData && req.session.userData.admin) { //If admin is connected
-        var redirectToAdmin = true;
-
-        var User = require('../models/user.js');
-        var Admin = require('../models/admin.js');
-        var Project = require('../models/project.js');
-        var Contact = require('../models/contact.js');
-        var parse = require('csv-parse/lib/sync');
-
-        if(req.body.submitButton == "createInter"){ //Create interviewer
-            User.createAccount(req, res);
-        }
-        else if(req.body.submitButton == "createProyect"){
-            Project.createNewProject(req, res);
-        }
-        else if(req.body.submitButton == "deleteInter"){
-            Admin.deleteUser(req.body.readUser, req.session.userData.userID, req.body.userDeletePass);
-            User.getAllUsers(req);
-        }
-        else if(req.body.submitButton == "uploadCSV"){
-             var proyectCsv = req.files.uploadContacts;
-             var csvParsed = parse(proyectCsv.data.toString());
-
-             for(var i = 1; i < csvParsed.length; i++){
-                Contact.createContact(csvParsed[i][0], csvParsed[i][1], csvParsed[i][2], csvParsed[i][3], req.body.uploadProject);
-             }
-        }
-        /*else if(req.body.submitButton == "readInter"){
-            redirectToAdmin = false;
-            res.redirect('/admin/verEncuestador/'+req.body.readUser);
-        }*/
-/*
-        if(redirectToAdmin){
-            res.redirect('/admin');
-        }
-    }
-    else{//else, redirects to the login interface
-        res.redirect('/login');
-    }*/
+exports.processForm = function(req, res){
     index.verificateLogin(req, res, true, function(req, res){
         var redirectToAdmin = true;
 
@@ -138,10 +70,6 @@ exports.processForm = function (req, res){
                 Contact.createContact(csvParsed[i][0], csvParsed[i][1], csvParsed[i][2], csvParsed[i][3], req.body.uploadProject);
             }
         }
-        /*else if(req.body.submitButton == "readInter"){
-            redirectToAdmin = false;
-            res.redirect('/admin/verEncuestador/'+req.body.readUser);
-        }*/
 
         if(redirectToAdmin){
             res.redirect('/admin');
--- cati-sw/routes/user.js
+++ cati-sw/routes/user.js
@@ -3,14 +3,6 @@ var index = require('../routes/index');
 
 //user interface
 exports.userInterface = function(req, res){
-    /*if(req.session.userData && !req.session.userData.admin){ //If user is connected
-        res.render('userDash', { title: 'CATI - Encuestador', nombre: req.session.userData.userName });
-    }
-    else{//else, redirects to the login interface
-        res.redirect('/login');
-    }*/
-    //index.verificateLogin(req, res, false, 'userDash', { title: 'CATI - Encuestador', nombre: req.session.userData.userName });
-
     index.verificateLogin(req, res, false, function(req, res){
         res.render('userDash', { title: 'CATI - Encuestador', nombre: req.session.userData.userName });
     });
@@ -18,13 +10,6 @@ exports.userInterface = function(req, res){
 };
 
 exports.changePassInterface = function(req, res){
-    /*if(req.session.userData && !req.session.userData.admin){ //If user is connected
-     res.render('updateUserDashUser', { title: 'CATI - Encuestador - Cambiar contraseña', nombre: req.session.userData.userName });
-     }
-     else{//else, redirects to the login interface
-     res.redirect('/login');
-     }*/
-    //index.verificateLogin(req, res, false, 'updateUserDashUser', { title: 'CATI - Encuestador - Cambiar contraseña', nombre: req.session.userData.userName });
     index.verificateLogin(req, res, false, function(req, res){
         res.render('updateUser', { title: 'CATI - Encuestador - Cambiar contraseña', nombre: req.session.userData.userName });
     });
@@ -32,18 +17,6 @@ exports.changePassInterface = function(req, res){
 
 //user form handler
 exports.processForm = function(req, res){
-    /*if(req.session.userData && !req.session.userData.admin) { //If user is connected
-        if(req.body.submitButton == "changePass"){ //Change the pass interviewer
-            if(req.body.interNewPass == req.body.interNewPass2){
-                var User = require('../models/user.js');
-                User.changePass(req.session.userData.userID, req.body.interOldPass, req.body.interNewPass);
-            }
-        }
-        res.redirect('/user');
-    }
-    else{//else, redirects to the login interface
-        res.redirect('/login');
-    }*/
     index.verificateLogin(req, res, false, function(req, res){
         if(req.body.submitButton == "changePass"){ //Change the pass interviewer
             if(req.body.interNewPass == req.body.interNewPass2){
